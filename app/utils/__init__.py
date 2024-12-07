9d778468947e24c9468c33dbc57b12eedb7e44ee
Babahasko <tartilik@yandex.ru>
2024-12-07 13:33:26 +0300

    add streamlit custom addon

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,7 +1,11 @@
 __all__ = (
     "sort_columns",
-    "logger"
+    "logger",
+    "validate_data",
+    "custom_info"
 )
 
 from .filter_functions import sort_columns
 from .logger import logger
+from .validate_function import validate_data
+from .streamlit_addons import custom_info
