1aa1c822bec893628fd55aa392747d9f2574d450
Babahasko <tartilik@yandex.ru>
2024-12-07 16:57:49 +0300

    back to the working validation function

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,11 +3,9 @@ __all__ = (
     "logger",
     "validate_data",
     "custom_info",
-    "load_validation_input_config",
 )
 
 from .filter_functions import sort_columns
 from .logger import logger
 from .validate_function import validate_data
 from .streamlit_addons import custom_info
-from .config import load_validation_input_config
--- app/utils/config.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-
-def load_validation_input_config():
-    with open('validation_input_config.json', 'r') as file:
-        validation_input_config = json.load(file)
-    return validation_input_config
