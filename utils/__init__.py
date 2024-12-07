e00f25ac31d0cf19f1565ea973606aec67dac287
Babahasko <tartilik@yandex.ru>
2024-12-07 22:24:01 +0300

    add new utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,9 @@ __all__ = (
     "logger",
     "validate_data",
     "custom_info",
-    "model_pipeline"
+    "model_pipeline",
+    "round_for_restrictions",
+    "show_colored_optimization"
 )
 
 from .filter_functions import sort_columns
@@ -11,3 +13,4 @@ from .logger import logger
 from .validate_function import validate_data
 from .streamlit_addons import custom_info
 from .model_alogrithm import model_pipeline
+from .model_algorithm_helper import round_for_restrictions, show_colored_optimization
